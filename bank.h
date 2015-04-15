8ca7b119106081cea4cc615e27676314edcb7c3d
FLOYDfittyseven <FLOYDfittyseven@users.noreply.github.com>
2015-04-14 21:45:59 -0400

    Update bank.h

--- bank.h
+++ bank.h
@@ -1,1 +1,33 @@
+typedef enum { false, true } bool;
+
+struct account
+{
+	char accountname[101];
+	float balance;
+	bool flag;
+};
+
+struct account *
+CreateAccount( char * accountname );
+
+void
+DestroyAccount( struct account * a );
+
+int
+create_bank_account( struct account ** bank, char * accountname, int in_session, char ** message );
+
+int
+serve_account( struct account ** bank, char * accountname );
+
+int
+deposit( struct account ** bank, char * accountname, float amount );
+
+int
+withdraw( struct account ** bank, char * accountname, float amount );
+
+float
+query( struct account ** bank, char * accountname );
+
+int
+end( struct account ** bank, char * accountname );
 
