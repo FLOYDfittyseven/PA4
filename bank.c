0513b8e682e17b69a0ed124fae4f25b89ffdfbc1
FLOYDfittyseven <FLOYDfittyseven@users.noreply.github.com>
2015-04-16 19:27:38 -0400

    Update bank.c

--- bank.c
+++ bank.c
@@ -15,16 +15,22 @@ CreateAccount( char * accountname )
 	}
 	
 	if( (ret = (struct account *)malloc( sizeof(struct account) )) == 0)
-		{
-			printf( "malloc() failed in %s line %d.\n", __FILE__, __LINE__);
-			return NULL;
-		}
+	{
+		printf( "malloc() failed in %s line %d.\n", __FILE__, __LINE__);
+		return NULL;
+	}
+	else if( (ret->accountname = (char *)malloc(101 * sizeof(char) )) == 0)
+	{
+		printf( "malloc() failed in %s line %d.\n", __FILE__, __LINE__);
+		free( ret );
+		return NULL;
+	}
 	
-	memcpy(ret->accountname, "\0", sizeof(ret->accountname));
-	memcpy(ret->accountname, accountname, sizeof(accountname));
-	
+	strcpy(ret->accountname, accountname);
+	ret->accountname[100] = '\0';
 	ret->balance = 0;
 	ret->flag = false;
+	pthread_mutex_init( &ret->mutex, NULL );
 	
 	return ret;
 }
@@ -36,10 +42,71 @@ DestroyAccount( struct account * a )
 	{
 		return;
 	}
-	
+	free( a->accountname );
 	free( a );
 }
 
+
+struct bank *
+CreateBank()
+{
+	struct bank * b;
+	int i;
+	
+	if( (b = (struct bank *)malloc( sizeof(struct bank) )) == 0)
+	{
+		printf( "malloc() failed in %s line %d.\n", __FILE__, __LINE__);
+		return NULL;
+	}
+	else if( (b->accounts = (struct account **)malloc(20 * sizeof(struct account *) )) == 0)
+	{
+		printf( "malloc() failed in %s line %d.\n", __FILE__, __LINE__);
+		return NULL;
+	}
+	
+	for( i=0; i<20; i++ )
+	{
+		b->accounts[i] = NULL;
+	}
+	
+	b->numAccounts = 0;
+	pthread_mutex_init( &b->mutex, NULL );
+	b->emptyslot = b->accounts;
+	b->active = NULL;
+	
+	return b;
+}
+
+
+void
+DestroyBank( struct bank * b )
+{
+	
+	while( b->numAccounts > 0 )
+	{
+		DestroyAccount( b->accounts[b->numAccounts] );
+		b->numAccounts--;
+	}
+	free( b->accounts );
+	free( b );
+	return;
+}
+
+struct account *
+FindAccount( char * name, struct bank * Bank )
+{
+	int i;
+	
+	for( i = 0; i < Bank->numAccounts; i++ )
+	{
+		if( strcmp( Bank->accounts[i]->accountname, name ) == 0 )
+		{
+			return Bank->accounts[i];
+		}
+	}
+	
+	return NULL;
+}
 
 /*
  * create_bank_account takes a pointer to the array of accounts (the bank)
@@ -51,11 +118,11 @@ DestroyAccount( struct account * a )
  * trailing null byte).
  */
 int
-create_bank_account( struct account ** bank, pthread_mutex_t ** mutexes, char * accountname, int in_session, char ** message )
+create_bank_account( struct bank * Bank, char * accountname, int in_session, char ** message )
 {
-	int i;
+	struct account *ptr;
 	
-	if( !bank | !accountname | !mutexes )
+	if( !Bank | !accountname )
 	{
 		return -1;
 	}
@@ -64,42 +131,35 @@ create_bank_account( struct account ** bank, pthread_mutex_t ** mutexes, char * 
 		return sprintf( *message, "You may not create accounts while in a customer session.\n" );
 	}
 	
-	pthread_mutex_lock( mutexes[0] );
-	for( i=0; i<20; i++ )
-	{
-		if( !bank[i] )
-		{
-			break;
-		}
-		else if( strcmp( accountname, bank[i]->accountname ) == 0 )
-		{
-			pthread_mutex_unlock( mutexes[0] );
-			return sprintf( *message, "Account \"%s\" already exists.\n", accountname );
-		}
-		else
-		{
-			continue;
-		}
-	}
+	pthread_mutex_lock( &Bank->mutex );
 	
-	if( i == 20)
+	if( Bank->numAccounts == 20 )
 	{
-		pthread_mutex_unlock( mutexes[0] );
+		pthread_mutex_unlock( &Bank->mutex );
 		return sprintf( *message, "Bank is full.\n" );
 	}
-	
-	bank[i] = CreateAccount( accountname );
-	pthread_mutex_unlock( mutexes[0] );
-	return sprintf( *message, "Account \"%s\" successfully created.\n", accountname );
+	else if( (ptr = FindAccount( accountname, Bank )) )
+	{
+		pthread_mutex_unlock( &Bank->mutex );
+		return sprintf( *message, "Account \"%s\" already exists.\n", ptr->accountname );
+	}
+	else
+	{
+		*(Bank->emptyslot) = CreateAccount( accountname );
+		Bank->emptyslot++;
+		Bank->numAccounts++;
+		pthread_mutex_unlock( &Bank->mutex );
+		return sprintf( *message, "Account \"%s\" successfully created.\n", accountname );
+	}
 }
 
 
 int
-serve_account( struct account ** bank, pthread_mutex_t ** mutexes, char * accountname, int * in_session, char ** message, char ** session_name )
+serve_account( struct bank * Bank, char * accountname, int * in_session, char ** message, char ** session_name )
 {
-	int i;
+	struct account *ptr;
 	
-	if( !bank | !accountname | !mutexes )
+	if( !Bank | !accountname )
 	{
 		return -1;
 	}
@@ -107,42 +167,100 @@ serve_account( struct account ** bank, pthread_mutex_t ** mutexes, char * accoun
 	{
 		return sprintf( *message, "You must end the current customer session (%s) before serving an account.\n", *session_name );
 	}
-	
-	for( i=0; i<20; i++ )
+	else if( (pthread_mutex_lock( &Bank->mutex )), !(ptr = FindAccount( accountname, Bank )) )
 	{
-		if( !bank[i] )
-		{
-			break;
-		}
-		else if( strcmp( accountname, bank[i]->accountname ) == 0 )
-		{
-			pthread_mutex_lock( mutexes[i+1] );
-			*in_session = 1;
-			strcpy( *session_name, accountname );
-			return sprintf( *message, "Now serving account \"%s\".\n", accountname );
-		}
-		else
-		{
-			continue;
-		}
+		pthread_mutex_unlock( &Bank->mutex );
+		return sprintf( *message, "Account \"%s\" does not exist.\n", accountname );
+	}
+	else if( (pthread_mutex_unlock( &Bank->mutex )), (pthread_mutex_lock( &ptr->mutex )) == 0 )
+	{
+		*in_session = 1;
+		strcpy( *session_name, accountname );
+		ptr->flag = true;
+		Bank->active = ptr;
+		return sprintf( *message, "Now serving account \"%s\".\n", accountname );
 	}
 	
-	return sprintf( *message, "Account \"%s\" does not exist.\n", accountname );
+	return -1;
+}
+
+int
+end( struct bank * Bank, int * in_session, char ** message, char * session_name )
+{
+	if( !Bank )
+	{
+		return -1;
+	}
+	else if( !(*in_session) )
+	{
+		return sprintf( *message, "You do not currently have an active customer session.\n" );
+	}
+	else
+	{
+		*in_session = 0;
+		Bank->active->flag = false;
+		pthread_mutex_unlock( &Bank->active->mutex );
+		Bank->active = NULL;
+		return sprintf( *message, "Successfully ended customer session \"%s\".\n", session_name );
+	}
+}
+
+int
+deposit( struct bank * Bank, float amount, char ** message )
+{
+	if( !Bank )
+	{
+		return -1;
+	}
+	else if( !(Bank->active) )
+	{
+		return sprintf( *message, "You do not currently have an active customer session.\n" );
+	}
+	else if( amount < 0 )
+	{
+		return sprintf( *message, "Nice try.\n" );
+	}
+	else
+	{
+		Bank->active->balance += amount;
+		return sprintf( *message, "Successfully deposited $%.2f to account \"%s\".\n", amount,
+		Bank->active->accountname );
+	}
+}
+
+int
+withdraw( struct bank * Bank, float amount, char ** message )
+{
+	if( !Bank )
+	{
+		return -1;
+	}
+	else if( !(Bank->active) )
+	{
+		return sprintf( *message, "You do not currently have an active customer session.\n" );
+	}
+	else if( amount < 0 )
+	{
+		return sprintf( *message, "You may not withdraw a negative amount.\n" );
+	}
+	else if( amount > Bank->active->balance )
+	{
+		return sprintf( *message, "Insufficient funds.\n" );
+	}
+	else
+	{
+		Bank->active->balance -= amount;
+		return sprintf( *message, "Successfully withdrew $%.2f from account \"%s\".\n", amount,
+		Bank->active->accountname );
+	}
 }
 
 /*
-int deposit( struct account * bank, char * accountname, float amount )
-{
-}
-
-int withdraw( struct account * bank, char * accountname, float amount )
-{
-}
-
 float query( struct account * bank, char * accountname )
 {
 }
 
-int end( struct account * bank, char * accountname )
+int
+quit()
 {
 }*/
